813ff1750c49686af4f4e75bc6c7a95dcb3f9127
Bahar <bahar@regentmarkets.com>
2023-05-09 10:38:35 +0800

    use container

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100,
+            width: 100,
+            margin: EdgeInsets.fromLTRB(30, 10, 50, 20),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text("Hello"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
